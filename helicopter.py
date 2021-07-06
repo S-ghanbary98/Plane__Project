bf4edcdbc9436b7df8dc669cd90d68c66a10d7f3
Connor <connorhaylers@gmail.com>
2021-07-06 12:08:49 +0100

    fixed syntax

--- helicopter.py
+++ helicopter.py
@@ -3,4 +3,4 @@ from aircraft import Aircraft
 
 class Helicopter(Aircraft):
     def __init__(self, capacity, age, fuel_capacity):
-        super.__init__(capacity, age, "HELICOPTER", fuel_capacity)
+        super().__init__(capacity, age, "HELICOPTER", fuel_capacity)
--- plane.py
+++ plane.py
@@ -3,4 +3,4 @@ from aircraft import Aircraft
 
 class Plane(Aircraft):
     def __init__(self, capacity, age, fuel_capacity):
-        super.__init__(capacity, age, "PLANE", fuel_capacity)
+        super().__init__(capacity, age, "PLANE", fuel_capacity)
